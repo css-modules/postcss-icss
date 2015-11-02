f6b1050cf1fce60949aa0b5ef92828ee29008fa3
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-11-02 23:37:04 +0300

    initial

---
+++ readme.md
@@ -0,0 +1,6 @@
+postcss-modules-parser
+======================
+
+A CSS Modules parser to extract tokens from the css file. Supports both synchronous and asynchronous file loaders.
+
+Soon will be released.
---
+++ src/index.js
@@ -0,0 +1,72 @@
+import { plugin } from 'postcss';
+import forEach from 'lodash.foreach';
+import replaceSymbols from 'icss-replace-symbols';
+const importRegexp = /^:import\((.+)\)$/;
+const exportRegexp = /^:export$/;
+
+/**
+ * @param  {object}  promise
+ * @return {boolean}
+ */
+function isPromise(promise) {
+  return typeof promise === 'object' && typeof promise.then === 'function';
+}
+
+/**
+ * @param  {object} css
+ * @param  {object} translations
+ * @param  {object} exportTokens
+ */
+function proceed(css, translations, exportTokens) {
+  replaceSymbols(css, translations);
+
+  css.walkRules(exportRegexp, rule => {
+    rule.walkDecls(decl => {
+      forEach(translations, (value, key) => decl.value = decl.value.replace(key, value));
+      exportTokens[decl.prop] = decl.value;
+    });
+
+    rule.remove();
+  });
+
+  css.tokens = exportTokens;
+}
+
+/**
+ * @param  {function} options.fetch
+ * @return {function}
+ */
+export default plugin('parser', function parser({ fetch } = {}) {
+  return css => {
+    // https://github.com/postcss/postcss/blob/master/docs/api.md#inputfile
+    const file = css.source.input.file;
+
+    const translations = {};
+    const promises = [];
+
+    css.walkRules(importRegexp, rule => {
+      const result = fetch(RegExp.$1, file);
+
+      if (isPromise(result)) {
+        result.then(exports => {
+          rule.walkDecls(decl => translations[decl.prop] = exports[decl.value]);
+          rule.remove();
+        });
+
+        promises.push(result);
+      } else {
+        rule.walkDecls(decl => translations[decl.prop] = result[decl.value]);
+        rule.remove();
+      }
+    });
+
+    const exportTokens = {};
+
+    if (promises.length) {
+      return Promise.all(promises)
+        .then(() => proceed(css, translations, exportTokens));
+    }
+
+    proceed(css, translations, exportTokens);
+  };
+});
