c25f6a3ce57c87f0836caa1e39ededab073cf4e1
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-11-04 00:43:48 +0300

    asynchronous test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 import { equal } from 'assert';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
+import asyncLoader from './helper/async-loader';
 import syncLoader from './helper/sync-loader';
 
 let fixture;
@@ -9,9 +10,18 @@ let filename;
 
 describe('postcss-modules-parser', _ => {
   beforeEach(() => {
-    fixture = 'test/fixture';
+    fixture = 'test/fixture/simple';
     filename = resolve(fixture, 'source.css');
     expected = JSON.parse(readFileSync(resolve(fixture, 'expected.json'), 'utf8'));
+  });
+
+  it('asynchronous', done => {
+    const result = asyncLoader(filename, filename)
+      .then(result => {
+        equal(JSON.stringify(result), JSON.stringify(expected));
+        done();
+      })
+      .catch(done);
   });
 
   it('synchronous', () => {
